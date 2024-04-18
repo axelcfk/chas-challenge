519941c1563b3f41be7b33d4fd818eb5e1fabf45
axelcfk <131769961+axelcfk@users.noreply.github.com>
2024-04-18 13:08:36 +0200

    la till template för fetch från api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
-import mysql from "mysql2/promise";
 import cors from "cors";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 3010;
 
 //Middleware
 
@@ -11,8 +13,25 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+const openAI = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+dotenv.config();
+
+app.get("/joke", async (req, res) => {
+  const apiKey = process.env.OPENAI_API_KEY;
+
+  const completion = await openAI.chat.completions.create({
+    messages: [{ role: "user", content: "Tell me a joke" }],
+    model: "gpt-3.5-turbo",
+  });
+
+  const result = completion.choices[0].message.content;
+
+  res.send(result);
+});
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the Banking Backend!");
+  res.send("Welcome to the backend!");
 });
 
 app.listen(port, "0.0.0.0", () => {
