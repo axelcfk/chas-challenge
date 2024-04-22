ff3589e6c0dbe8274778c5696d8422e3b529dc82
maek95 <mekstrom01@gmail.com>
2024-04-22 13:04:48 +0200

    mysql store userquery and ai_response (user_id = 1)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import cors from "cors";
 import OpenAI from "openai";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
 const port = 3010;
+
+// connect to DB
+const pool = mysql.createPool({
+  //host: "mysql",
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "health-app",
+});
+
+// help function to make code look nicer
+async function query(sql, params) {
+  const [results] = await pool.execute(sql, params); // får en array, så måste vara [results]...?
+  return results;
+}
 
 //Middleware
 
@@ -44,6 +60,21 @@ app.post("/joke", async (req, res) => {
 
     //skickar result tillbaka till frontend
     res.json({ joke: result });
+
+    // STORING USERQUERY AND AIRESPONSE TO MYSQL
+    const user_id = 1; // PLACEHOLDER USER_ID
+    try {
+      // create an empty account (0 kr) for the user, with the user_id attached to it
+      const userQueryAndResponseSQLData = await query(
+        "INSERT INTO chatgpt (user_id, user_query, ai_response) VALUES (?, ?, ?)",
+        [user_id, userQuery, result]
+      );
+      console.log("userQuerySQLData: ", userQueryAndResponseSQLData[0]);
+    } catch (error) {
+      console.error("Error saving user query and ai response to mysql: ", error);
+      return res.status(500).send("Error saving user query and ai response to mysql");
+    }
+
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({
@@ -51,6 +82,9 @@ app.post("/joke", async (req, res) => {
       details: error.message,
     });
   }
+  
+  
+
 });
 
 app.listen(port, "0.0.0.0", () => {
